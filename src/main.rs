ebbe70ad02a956dff391974d2a5a497d3a807317
Aamir Siddiqui <aamir_siddiqui17@yahoo.com>
2024-12-02 21:20:01 +0000

    write compute shader with collatz conjecture

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,136 @@
-async fn run() {
-    #[cfg_attr(target_arch = "wasm32", allow(unused_variables))]
-    let adapter = {
-        let instances = wgpu::Instance::default();
-        
-        #[cfg(not(target_arch = "wasm32"))]
-        {
-            log::info!("Available adapters:");
-            for a in instances.enumerate_adapters(wgpu::Backends::all()) {
-                log::info!("    {:?}", a.get_info())
-            }
-        }
-        instances
-        .request_adapter(&wgpu::RequestAdapterOptionsBase::default())
-        .await
-        .unwrap()   
-    };
+use wgpu::util::DeviceExt;
 
-    log::info!("Selected adapter: {:?}", adapter.get_info());
+const OVERFLOW: u32 = 0xffffffff;
+
+async fn run() {
+    let numbers = vec![76324, 4243, 312, 2956453];
+
+    let steps = execute_gpu(&numbers).await.unwrap();
+
+    let disp_step: Vec<String> = steps
+        .iter()
+        .map(|&n| match n {
+            OVERFLOW => "OVERFLOW".to_string(),
+            _ => n.to_string(),
+        })
+        .collect();
+
+    println!("Steps: [{}]", disp_step.join(", "));
+
+    #[cfg(target_arch = "wasm32")]
+    log::info!("Steps: [{}]", disp_step.join(", "));
 }
 
+async fn execute_gpu(numbers: &[u32]) -> Option<Vec<u32>> {
+    let instance = wgpu::Instance::default();
+
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions::default())
+        .await?;
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::downlevel_defaults(),
+                memory_hints: wgpu::MemoryHints::MemoryUsage,
+            },
+            None,
+        )
+        .await
+        .unwrap();
+
+    execute_gpu_inner(&device, &queue, numbers).await
+}
+
+async fn execute_gpu_inner(
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+    numbers: &[u32],
+) -> Option<Vec<u32>> {
+    let cs_module = device.create_shader_module(wgpu::include_wgsl!("shader.wsgl"));
+
+    let size = size_of_val(numbers) as wgpu::BufferAddress;
+
+    let staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: None,
+        size,
+        usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    });
+
+    let storage_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Storage Buffer"),
+        contents: bytemuck::cast_slice(numbers),
+        usage: wgpu::BufferUsages::STORAGE
+            | wgpu::BufferUsages::COPY_DST
+            | wgpu::BufferUsages::COPY_SRC,
+    });
+
+    let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label: None,
+        layout: None,
+        module: &cs_module,
+        entry_point: Some("main"),
+        compilation_options: Default::default(),
+        cache: None,
+    });
+
+    let bind_group_layout = compute_pipeline.get_bind_group_layout(0);
+
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: &bind_group_layout,
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: storage_buffer.as_entire_binding(),
+        }],
+    });
+
+    let mut encoder =
+        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+    {
+        let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: None,
+            timestamp_writes: None,
+        });
+
+        cpass.set_pipeline(&compute_pipeline);
+        cpass.set_bind_group(0, &bind_group, &[]);
+        cpass.insert_debug_marker("compute collatz iterations");
+        cpass.dispatch_workgroups(numbers.len() as u32, 1, 1);
+    }
+
+    encoder.copy_buffer_to_buffer(&storage_buffer, 0, &staging_buffer, 0, size);
+
+    queue.submit(Some(encoder.finish()));
+
+    let buffer_slice = staging_buffer.slice(..);
+
+    let (sender, receiver) = flume::bounded(1);
+    buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+
+    device.poll(wgpu::Maintain::wait()).panic_on_timeout();
+
+    if let Ok(Ok(())) = receiver.recv_async().await {
+        let data = buffer_slice.get_mapped_range();
+
+        let result = bytemuck::cast_slice(&data).to_vec();
+
+        drop(data);
+        staging_buffer.unmap();
+
+        Some(result)
+    } else {
+        panic!("Failed to run compute on gpu!")
+    }
+}
 
 fn main() {
     #[cfg(not(target_arch = "wasm32"))]
     {
-        env_logger::builder()
-        .parse_default_env()
-        .init();
+        env_logger::builder().parse_default_env().init();
         pollster::block_on(run());
     }
     #[cfg(target_arch = "wasm32")]
